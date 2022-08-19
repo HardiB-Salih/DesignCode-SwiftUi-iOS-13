47107ccbccffc785990d56f6a8ffab0e15b989ac
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2022-08-19 18:48:29 +0300

    Initial Commit

---
+++ DesignCodeSwiftUi-iOS13/DesignCodeSwiftUi_iOS13App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeSwiftUi_iOS13App.swift
+//  DesignCodeSwiftUi-iOS13
+//
+//  Created by HardiBSalih on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeSwiftUi_iOS13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
