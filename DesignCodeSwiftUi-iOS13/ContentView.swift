3a95b811783b84e68f937bc7bcac4dbaf94d4fda
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2022-08-20 12:38:59 +0300

    Dismiss and Drag Anchors

--- DesignCodeSwiftUi-iOS13/ContentView.swift
+++ DesignCodeSwiftUi-iOS13/ContentView.swift
@@ -11,42 +11,64 @@ struct ContentView: View {
     
     @State var show = false
     @State var viewState = CGSize.zero
+    @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
+
+
     var body: some View {
         ZStack {
             TitleView()
                 .blur(radius: show ? 20 : 0)
-                .animation(.default, value: show)
+                .opacity(showCard ? 0.4 : 1)
+                .offset(y: showCard ? -200 : 0)
+                .animation(Animation.default.delay(0.1), value: showCard)
             
             BackCardView()
+                .frame(width: showCard ? 300 : 340, height: 220)
                 .background(show ? Color("card3") : Color("card4"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -400 : -40)
                 .offset(x: viewState.width, y: viewState.height)
-                .scaleEffect(0.9)
+                .offset(y: showCard ? -180 : 0)
+                .scaleEffect(showCard ? 1 : 0.9)
                 .rotationEffect(.degrees(show ? 0: 10))
-                .rotation3DEffect(.degrees(10), axis: (x: 10, y: 0, z: 0))
+                .rotationEffect(.degrees(showCard ? -10 : 0))
+                .rotation3DEffect(.degrees(showCard ? 0 : 10), axis: (x: 10, y: 0, z: 0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.5), value: show)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8), value: showCard)
             
             BackCardView()
+                .frame(width: 340, height: 220)
                 .background(show ? Color("card4") : Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -200 : -20)
                 .offset(x: viewState.width, y: viewState.height)
-                .scaleEffect(0.95)
+                .offset(y: showCard ? -140 : 0)
+                .scaleEffect(showCard ? 1 : 0.95)
                 .rotationEffect(.degrees(show ? 0 : 5))
-                .rotation3DEffect(.degrees(5), axis: (x: 10, y: 0, z: 0))
+                .rotationEffect(.degrees(showCard ? -5 : 0))
+                .rotation3DEffect(.degrees(showCard ? 0 : 5), axis: (x: 10, y: 0, z: 0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.3), value: show)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8), value: showCard)
             
             CardView()
+                .frame(width: showCard ? .infinity : 340, height: 220)
+                .background(.black)
+//                .cornerRadius(20)
+                .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
+                .shadow(radius: 20)
                 .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -90 : 0)
                 .blendMode(.hardLight)
                 .animation(.spring(response: 0.3, dampingFraction: 0.6), value: viewState)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8), value: showCard)
                 .onTapGesture {
-                    show.toggle()
+                    showCard.toggle()
                 }
                 .gesture(DragGesture().onChanged { value in
                     viewState = value.translation
@@ -58,8 +80,37 @@ struct ContentView: View {
                 )
             
             BottomCardView()
+                .offset(x: 0, y: showCard ? 400 : 1000)
+                .offset(y: bottomState.height)
                 .blur(radius: show ? 20 : 0)
-                .animation(.default, value: show)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8), value: showCard)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6), value: bottomState)
+                .gesture(DragGesture().onChanged{ value in
+                    bottomState = value.translation
+                    if showFull {
+                        bottomState.height += -350
+                    }
+                    
+                    if bottomState.height < -350 {
+                        bottomState.height = -350
+                    }
+                    
+                }.onEnded { value in
+                    if bottomState.height > 50 {
+                        showCard = false
+                    }
+                    
+                    if (bottomState.height < -100 && !showFull) || (bottomState.height < -250 && showFull) {
+                        bottomState.height = -350
+                        showFull = true
+                    } else {
+                        bottomState = .zero
+                        showFull = false
+                    }
+                    
+                    
+                })
+            
             
         }
     }
@@ -92,10 +143,7 @@ struct CardView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 300, height: 110, alignment: .top)
-        }.frame(width: 340, height: 220)
-            .background(.black)
-            .cornerRadius(20)
-            .shadow(radius: 20)
+        }
     }
 }
 
@@ -103,7 +151,7 @@ struct BackCardView: View {
     var body: some View {
         VStack {
             Spacer()
-        }.frame(width: 340, height: 220)
+        }
             
     }
 }
@@ -119,6 +167,9 @@ struct TitleView: View {
             }
             .padding()
             Image("Background1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: .infinity, height: 120, alignment: .top)
             Spacer()
             
         }
@@ -146,6 +197,6 @@ struct BottomCardView: View {
         .background(.white)
         .cornerRadius(30)
         .shadow(radius: 20)
-        .offset(x: 0, y: 550)
+        
     }
 }
